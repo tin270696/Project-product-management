6b61cd6165a69b277187993d2a613ade16374adc
tin270696 <lttin96@gmail.com>
2025-04-02 18:11:01 +0700

    Thêm middleware auth cho trang admin

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -6,21 +6,46 @@ const productCategoryRoutes = require("./product-category.route");
 const roleRoutes = require("./roles.route");
 const accountRoutes = require("./accounts.route");
 const authRoutes = require("./auth.route");
+const authMiddlewares = require("../../middlewares/admin/auth.middleware");
 
 module.exports = (app) => {
     const PATH_ADMIN = `/${systemConfig.prefixAdmin}`;
 
-    app.use(PATH_ADMIN + "/dashboard", dashboardRoutes);
+    app.use(
+        PATH_ADMIN + "/dashboard",
+        authMiddlewares.requireAuth,
+        dashboardRoutes
+    );
 
-    app.use(PATH_ADMIN + "/products", productRoutes);
+    app.use(
+        PATH_ADMIN + "/products",
+        authMiddlewares.requireAuth,
+        productRoutes
+    );
 
-    app.use(PATH_ADMIN + "/trash-can", trashCanRoutes);
+    app.use(
+        PATH_ADMIN + "/trash-can",
+        authMiddlewares.requireAuth,
+        trashCanRoutes
+    );
 
-    app.use(PATH_ADMIN + "/product-category", productCategoryRoutes);
+    app.use(
+        PATH_ADMIN + "/product-category",
+        authMiddlewares.requireAuth,
+        productCategoryRoutes
+    );
 
-    app.use(PATH_ADMIN + "/roles", roleRoutes);
+    app.use(
+        PATH_ADMIN + "/roles",
+        authMiddlewares.requireAuth,
+        roleRoutes
+    );
 
-    app.use(PATH_ADMIN + "/accounts", accountRoutes);
+    app.use(
+        PATH_ADMIN + "/accounts",
+        authMiddlewares.requireAuth,
+        accountRoutes
+    );
 
     app.use(PATH_ADMIN + "/auth", authRoutes);
 }
