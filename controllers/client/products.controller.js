0728fff9604649eb843ad9238f0f5793a8f31b6f
tin270696 <lttin96@gmail.com>
2025-04-04 22:22:37 +0700

    Hiển thị sản phẩm theo category bên client

--- controllers/client/products.controller.js
+++ controllers/client/products.controller.js
@@ -1,4 +1,5 @@
 const Product = require("../../models/product.model");
+const ProductCategory = require("../../models/product-category.model");
 
 // [GET] /products
 module.exports.index = async (req, res) => {
@@ -41,4 +42,52 @@ module.exports.detail = async (req, res) => {
         res.redirect("/products");
     }
     
+}
+
+// [GET] /products/:slugCategory
+module.exports.category = async (req, res) => {
+    const slugCategory = req.params.slugCategory;
+
+    const category = await ProductCategory.findOne({
+        slug: slugCategory,
+        deleted: false,
+        status: "active"
+    })
+
+    const getSubCategory = async (parent_id) => {
+        let allSubs = [];
+
+        const listSub = await ProductCategory.find({
+            parent_id: parent_id,
+            deleted: false,
+            status: "active"
+        }).select("id title");
+
+        allSubs = [...listSub];
+
+        for(const sub of listSub) {
+            const childs = await getSubCategory(sub.id);
+            allSubs = allSubs.concat(childs);
+        }
+
+        return allSubs;
+    }
+
+    const listSubCategory = await getSubCategory(category.id);
+    const listIdSubCategory = listSubCategory.map(item => item.id);
+
+    const products = await Product.find({
+        product_category_id: { $in: [category.id, ...listIdSubCategory] },
+        deleted: false,
+        status: "active"
+    }).sort({position: "desc"});
+
+    for(const item of products) {
+        item.newPrice = (item.price * (100 - item.discountPercentage)/100).toFixed(0);
+    }
+
+    res.render("client/pages/products/index", {
+        pageTitle: category.title,
+        products: products
+    })
 }
