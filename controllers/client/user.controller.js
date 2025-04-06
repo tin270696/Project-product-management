3ee52da57624f67b577dbf43b0ec5e2dcbf6d200
tin270696 <lttin96@gmail.com>
2025-04-06 23:44:52 +0700

    Trang đăng ký + đăng nhập

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -6,7 +6,7 @@ const generateHelper = require("../../helpers/generate.helper");
 // [GET] /user/register
 module.exports.register = (req, res) => {
     res.render("client/pages/user/register", {
-        pageTile: "Đăng ký tài khoản"
+        pageTitle: "Đăng ký tài khoản"
     })
 }
 
@@ -35,5 +35,44 @@ module.exports.registerPost = async (req, res) => {
 
     res.cookie("tokenUser", user.tokenUser);
 
+    res.redirect("/");
+}
+
+// [GET] /user/login/
+module.exports.login = (req, res) => {
+    res.render("client/pages/user/login", {
+        pageTitle: "Đăng nhập"
+    })
+}
+
+// [POST] /user/login/
+module.exports.loginPost = async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const user = await User.findOne({
+        email: email,
+        deleted: false
+    })
+
+    if(!user) {
+        req.flash("error", "Email không tồn tại!");
+        res.redirect("back");
+        return;
+    }
+
+    if(user.status != "active") {
+        req.flash("error", "Tài khoản bị khóa!");
+        res.redirect("back");
+        return;
+    }
+
+    if(md5(password) != user.password) {
+        req.flash("error", "Mật khẩu không chính xác!");
+        res.redirect("back");
+        return;
+    }
+
+    res.cookie("tokenUser", user.tokenUser);
     res.redirect("/");
 }
