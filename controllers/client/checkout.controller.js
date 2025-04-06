c5bf50a6fc3b1821b777bd9e6b2ba9df2c99bb24
tin270696 <lttin96@gmail.com>
2025-04-06 23:01:46 +0700

    Tạo trang checkout + gửi data khách và giỏ hàng về

--- controllers/client/checkout.controller.js
+++ controllers/client/checkout.controller.js
@@ -1,5 +1,6 @@
 const Cart = require("../../models/cart.model");
 const Product = require("../../models/product.model");
+const Order = require("../../models/order.model");
 
 // [GET] /checkout
 module.exports.index = async (req, res) => {
@@ -27,4 +28,82 @@ module.exports.index = async (req, res) => {
         pageTitle: "Đặt hàng",
         cartDetail: cart
     })
+}
+
+// [POST] /checkout/order
+module.exports.order = async (req, res) => {
+    const cartId = req.cookies.cartId;
+    const userInfo = req.body;
+
+    const cart = await Cart.findOne({
+        _id: cartId
+    })
+
+    const products = [];
+
+    for(const item of cart.products) {
+        const product = await Product.findOne({
+            _id: item.product_id,
+            deleted: false
+        })
+
+        const objectProduct = {
+            product_id: product.id,
+            price: product.price,
+            discountPercentage: product.discountPercentage,
+            quantity: item.quantity
+        }
+
+        products.push(objectProduct);
+    }
+
+    const dataOrder = {
+        // user_id: String,
+        cart_id: cartId,
+        userInfo: userInfo,
+        products: products
+    }
+
+    const order = new Order(dataOrder);
+    order.save();
+
+    await Cart.updateOne({
+        _id: cartId
+    },{
+        products: []
+    });
+
+    res.redirect(`/checkout/success/${order.id}`);
+}
+
+// [GET] /checkout/success/:orderId
+module.exports.success = async (req, res) => {
+    try {
+        const orderId = req.params.orderId;
+
+        const order = await Order.findOne({
+            _id: orderId
+        })
+
+        order.totalPrice = 0;
+
+        for(const product of order.products) {
+            const productInfo = await Product.findOne({
+                _id: product.product_id
+            })
+
+            product.title = productInfo.title;
+            product.thumbnail = productInfo.thumbnail;
+            product.newPrice = (product.price * (100 - product.discountPercentage)/100).toFixed(0);
+            product.totalPrice = product.newPrice * product.quantity;
+
+            order.totalPrice += product.totalPrice;
+        }
+        res.render("client/pages/checkout/success", {
+            pageTitle: "Đặt hàng thành công",
+            order: order
+        })
+    } catch (error) {
+        res.redirect("/");
+    }
 }
--- models/order.model.js
+++ models/order.model.js
@@ -15,7 +15,7 @@ const orderSchema = new mongoose.Schema({
             discountPercentage: Number,
             quantity: Number
         },
-    ],   
+    ],
 }, {
     timestamps: true
 })
