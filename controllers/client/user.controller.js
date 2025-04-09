79ec6846f4b642e19fb531e8b62637e120b47c9a
tin270696 <lttin96@gmail.com>
2025-04-09 20:18:03 +0700

    Đăng xuất

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -76,3 +76,9 @@ module.exports.loginPost = async (req, res) => {
     res.cookie("tokenUser", user.tokenUser);
     res.redirect("/");
 }
+
+// [GET] /user/logout
+module.exports.logout = (req, res) => {
+    res.clearCookie("tokenUser");
+    res.redirect("/");
+}
